abc050cbed4e00411fdb9c69b2f10ef4de2b3757
Henry <henrykurniawan1st@gmail.com>
2023-01-29 01:50:29 +0700

    Optimized by ChatGPT

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,8 @@
-// Modules to control application life and create native browser window
-const { app, globalShortcut, BrowserWindow, screen } = require('electron');
+const { app, globalShortcut, BrowserWindow } = require('electron');
 const path = require('path');
 
-var homePage = 'https://chat.openai.com';
-
-var userAgent =
+const homePage = 'https://chat.openai.com';
+const userAgent =
   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.101 Safari/537.36'; // Linux
 
 if (process.argv.includes('--spoof-chromeos')) {
@@ -26,11 +24,6 @@ app.commandLine.appendSwitch(
   'enable-features',
   'VaapiVideoDecoder,WaylandWindowDecorations'
 );
-
-app.commandLine.appendSwitch(
-  'disable-features',
-  'UseChromeOSDirectVideoDecoder'
-);
 app.commandLine.appendSwitch('enable-accelerated-mjpeg-decode');
 app.commandLine.appendSwitch('enable-accelerated-video');
 app.commandLine.appendSwitch('ignore-gpu-blacklist');
@@ -38,67 +31,40 @@ app.commandLine.appendSwitch('enable-native-gpu-memory-buffers');
 app.commandLine.appendSwitch('enable-gpu-rasterization');
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 1280,
     height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: false,
       userAgent: userAgent,
     },
   });
 
   mainWindow.loadURL(homePage);
   mainWindow.maximize();
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
+app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', async function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
-  globalShortcut.register('Alt+F4', async () => {
+  globalShortcut.register('Alt+F4', () => {
     app.quit();
   });
 
-  globalShortcut.register('F4', async () => {
+  globalShortcut.register('F4', () => {
     app.quit();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('browser-window-created', async function (e, window) {
+app.on('browser-window-created', (e, window) => {
   window.setBackgroundColor('#1A1D1F');
   window.setMenu(null);
-
-  window.webContents.setUserAgent(userAgent);
-
   window.webContents.on('new-window', (event, url) => {
     event.preventDefault();
     BrowserWindow.getAllWindows()[0].loadURL(url);
   });
 });
-
-app.on('will-quit', async () => {
-  globalShortcut.unregisterAll();
-});
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit();
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
